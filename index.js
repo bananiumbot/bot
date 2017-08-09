0695c48376e1d070dbf7720c496606d23cd1be7e
LexSerest <lexserest@gmail.com>
2017-08-09 11:34:07 +0300

    Update index.js
    add "/github" command

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Composer } = require('micro-bot')
 
 const bot = new Composer()
-bot.command('/start', ({ reply }) => reply('Hey!'))
-
+bot.command('/start', ({ reply }) => reply('Hey!'));
+bot.command('/github', ({reply}) => reply('github.com/bananiumbot/bot'));
 module.exports = bot
