a5de6ae3a69a2216273d05ad5cd4746c6df3edf0
Vitaly Domnikov <dotcypress@gmail.com>
2017-08-09 02:37:29 -0700

    only-admins

--- middlewares/only-admin.js
+++ middlewares/only-admin.js
@@ -1,7 +1,7 @@
 const debug = require('debug')('bananium:only-admin')
 
 module.exports = async ({ message, reply, telegram }, next) => {
-  const member = await telegram.getChatMember(message.chat.id).catch(debug)
+  const member = await telegram.getChatMember(message.chat.id, message.from.id).catch(debug)
   if (member && (member.status === 'creator' || member.status === 'administrator')) {
     return next()
   }
