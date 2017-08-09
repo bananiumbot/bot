931a15054c6bdb2fb0041be49f34f4ef403d052c
Vitaly Domnikov <dotcypress@gmail.com>
2017-08-09 02:34:17 -0700

    fix

--- middlewares/only-admin.js
+++ middlewares/only-admin.js
@@ -2,7 +2,7 @@ const debug = require('debug')('bananium:only-admin')
 
 module.exports = async ({ message, reply, telegram }, next) => {
   const member = await telegram.getChatMember(message.chat.id).catch(debug)
-  if (member && (member.status === 'creator' || member.status === 'creator')) {
+  if (member && (member.status === 'creator' || member.status === 'administrator')) {
     return next()
   }
   return reply('You shall not pass!')
