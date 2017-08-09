24ef71dba2e684e835c8139403a22697477f68bf
Lex Serest <lexserest@gmail.com>
2017-08-09 12:21:08 +0300

    add "/help" and "/github"

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const { Composer } = require('micro-bot')
+const { Composer, Markup, Extra } = require('micro-bot');
 
-const bot = new Composer()
-bot.command('/start', ({ reply }) => reply('Hey!'))
+const bot = new Composer();
+bot.command('/start', ({ reply }) => reply('Hey!'));
+
+bot.command('/github', ({ reply }) =>
+  reply('Исходники бота',
+    Markup.inlineKeyboard([
+      Markup.urlButton('GitHub', 'https://github.com/bananiumbot/bot'),
+    ]).extra()
+  )
+);
+
+bot.command('/help', ({ reply }) =>
+  reply(`Lorem ipsum dolor sit amet, consectetur adipiscing elit. <a href="https://github.com/bananiumbot/bot">Source</a>`,
+    Extra.webPreview(false).HTML()
+  )
+);
 
 module.exports = bot
