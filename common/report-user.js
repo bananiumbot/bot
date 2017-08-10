6450699b90895144a223a0118fdcb388c340684b
Vitaly Domnikov <dotcypress@gmail.com>
2017-08-10 12:33:36 -0700

    cleanup

--- common/report-user.js
+++ common/report-user.js
@@ -8,10 +8,7 @@ module.exports = async ({ message, reply, i18n, telegram }) => {
   await reply(i18n.t('report.done', { user: message.reply_to_message.from }), answerOptions)
   const admins = await telegram.getChatAdministrators(message.chat.id)
   const notification = i18n.t('report.notification')
-  const notificationOptions = Extra
-    .inReplyTo(message.message_id)
-    .HTML()
   await Promise.all(
-    admins.map(({ user }) => telegram.sendMessage(user.id, notification, notificationOptions).catch(() => {})
+    admins.map(({ user }) => telegram.sendMessage(user.id, notification, Extra.HTML()).catch(() => {})
   ))
 }
