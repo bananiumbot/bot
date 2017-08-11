98b781a7360dc5e9440a834e510964edb4ab144f
Vitaly Domnikov <dotcypress@gmail.com>
2017-08-11 00:44:54 -0700

    /ro

--- admin/restrict-user.js
+++ admin/restrict-user.js
@@ -1,3 +1,9 @@
-module.exports = ({ message, reply, i18n }) => message.reply_to_message
-  ? reply(i18n.t('admin.user-restricted', { user: message.reply_to_message.from }))
-  : reply(i18n.t('admin.restrict-help'))
+module.exports = async ({ message, reply, i18n, restrictChatMember }) => {
+  if (!message.reply_to_message) {
+    return reply(i18n.t('admin.restrict-help'))
+  }
+  const now = Math.floor(new Date().getTime() / 1000)
+  const targetUser = message.reply_to_message.from
+  await reply(i18n.t('admin.user-restricted', { user: targetUser }))
+  await restrictChatMember(targetUser.id, { until_date: now + 60 * 3 })
+}
