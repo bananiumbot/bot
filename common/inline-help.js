8eb7f5cfc527019aa01f76a612cfaf499bdf2d5a
Vitaly Domnikov <dotcypress@gmail.com>
2017-08-15 15:01:25 -0700

    add inline help

---
+++ common/inline-help.js
@@ -0,0 +1,27 @@
+const Fuse = require('fuse.js')
+const helpTopics = require('./bot-api-help.json')
+
+const fuse = new Fuse(helpTopics, {
+  shouldSort: true,
+  threshold: 0.2,
+  keys: ['title', 'description', 'meta']
+})
+
+module.exports = ({ inlineQuery, answerInlineQuery }) => {
+  const offset = parseInt(inlineQuery.offset) || 0
+  const results = fuse.search(inlineQuery.query)
+    .slice(offset, offset + 30)
+    .map(({ url, title, description }) => ({
+      type: 'article',
+      id: url,
+      url,
+      title,
+      description,
+      input_message_content: {
+        parse_mode: 'HTML',
+        message_text: `<b>${title}</b> – ${description}\n\n${url}`,
+        disable_web_page_preview: true
+      }
+    }))
+  return answerInlineQuery(results, {next_offset: offset + 30})
+}
