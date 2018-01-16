597ed3d90724be5b14f9ab080b62b643e5d7fbfe
Vitaly Domnikov <dotcypress@gmail.com>
2018-01-15 16:53:07 -0800

    cleanup

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,5 @@
-const { Composer, Markup } = require('micro-bot')
+const { Composer } = require('micro-bot')
 const fetch = require('node-fetch')
-const onlyAdmin = require('./../middlewares/only-admin')
 
 async function sendGag ({ replyWithVideo, i18n }) {
   const page = Math.floor(Math.random() * 100)
@@ -10,22 +9,8 @@ async function sendGag ({ replyWithVideo, i18n }) {
   return replyWithVideo(gag.videoURL, { caption: gag.description })
 }
 
-const HideKeyboardExtra = Markup.removeKeyboard().extra()
-const AuctionKeyboardExtra = Markup
-  .keyboard([
-    Markup.button('1BTC'),
-    Markup.button('3.50ETH'),
-    Markup.button('В личку написал'),
-    Markup.button('А есть ссылка на ТЗ?')
-  ])
-  .resize()
-  .extra()
-
 const bot = new Composer()
 
-bot.hears('!joke', sendGag)
-bot.hears('!auction', onlyAdmin, ({ reply }) => reply('Auction mode on', AuctionKeyboardExtra))
-bot.hears('!clear', onlyAdmin, ({ reply }) => reply('Auction mode off', HideKeyboardExtra))
 bot.hears('!joke', sendGag)
 
 module.exports = bot
