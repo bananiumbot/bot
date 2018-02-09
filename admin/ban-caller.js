f4db7abeabd9c3c0b12a3d85eedeb807d271f7f2
Vitaly Domnikov <dotcypress@gmail.com>
2018-02-09 14:08:48 -0800

    cleanup

--- admin/ban-caller.js
+++ admin/ban-caller.js
@@ -3,6 +3,5 @@ const { Extra } = require('micro-bot')
 module.exports = async (ctx) => {
   const now = Math.floor(new Date().getTime() / 1000)
   ctx.restrictChatMember(ctx.from.id, { until_date: now + 60 * 5 })
-  console.log('ctx.message.id', Extra.inReplyTo(ctx.message.id))
   return ctx.reply('Не вопрос!', Extra.inReplyTo(ctx.message.message_id))
 }
