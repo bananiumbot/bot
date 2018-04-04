71e219a7d42cb914576ef374745f09a6d6b9cdf8
Vitaly Domnikov <dotcypress@gmail.com>
2018-04-03 20:25:52 -0700

    update deps

--- common/handle-hashtag.js
+++ common/handle-hashtag.js
@@ -1,5 +1,5 @@
 const naiveDB = {
-  '#fsm': 'Python (пример с использованием pyTelegramBotAPI): https://groosha.gitbooks.io/telegram-bot-lessons/content/chapter11.html\naiogram: https://surik00.gitbooks.io/aiogram-lessons/content/chapter3.html\n\n #fsm',
+  '#fsm': 'Python (пример с использованием pyTelegramBotAPI): https://groosha.gitbooks.io/telegram-bot-lessons/content/chapter11.html\naiogram: https://surik00.gitbooks.io/aiogram-lessons/content/chapter3.html\n\n#fsm',
   '#webhook': 'https://core.telegram.org/bots/webhooks #webhook'
 }
 
@@ -8,7 +8,7 @@ module.exports = ({ message, reply }) => {
     .filter(({ type }) => type === 'hashtag')
     .map(({ offset, length }) => message.text.substring(offset, offset + length))
     .map((hashtag) => naiveDB[hashtag])
-    .filter(x => x)
+    .filter(Boolean)
     .join('\n')
   answer && reply(answer)
 }
