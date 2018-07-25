ae0d32e2282b5cfcba82c39100585a2b781bee22
megapascal <39701838+megapascal@users.noreply.github.com>
2018-07-25 15:24:01 +0500

    ///
    Will ban chinese bastards

--- admin/ban-bot.js
+++ admin/ban-bot.js
@@ -1,7 +1,14 @@
 module.exports = async ({ message, telegram, kickChatMember, replyWithSticker }) => {
-  const bots = message.new_chat_members.filter((member) => member.is_bot)
-  for (const bot of bots) {
-    await kickChatMember(bot.id)
-    await replyWithSticker('CAADAgADrQADVsXcB3r5IHrTHq6QAg')
+  const bots = message.new_chat_members.filter((member) => (
+    member.is_bot
+    || member.first_name.startsWith(`╋VX,QQ（同号）`)
+  ))
+
+  if (bots.length > 0) {
+    await context.deleteMessage()
+
+    for (const member of bots) {
+      await kickChatMember(member.id)
+    }
   }
 }
