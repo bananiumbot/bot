0eada81b41e4f1850bcf0887a78f5bb675cbf659
Vitaly Domnikov <dotcypress@gmail.com>
2018-10-07 14:24:53 -0700

    fix

--- common/inform-meta-user.js
+++ common/inform-meta-user.js
@@ -1,5 +1,6 @@
 const { Extra, optional } = require('micro-bot')
 
-module.exports = optional(({ message }) => message && message.reply_to_message,
+module.exports = optional(
+  ({ message }) => message && message.reply_to_message,
   ({ message, reply, i18n }) => reply(i18n.t('meta.done'), Extra.inReplyTo(message.reply_to_message.message_id))
 )
