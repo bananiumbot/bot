49372440034c7bc2e3e75e91a50b6d90106e3adf
Vitaly Domnikov <dotcypress@gmail.com>
2018-10-07 14:24:24 -0700

    fix

--- common/inform-meta-user.js
+++ common/inform-meta-user.js
@@ -1,8 +1,5 @@
-const { Extra } = require('micro-bot')
+const { Extra, optional } = require('micro-bot')
 
-module.exports = async ({ message, reply, i18n, telegram }) => {
-  const answerOptions = Extra.inReplyTo(message.message_id)
-  if (message.reply_to_message) {
-    return reply(i18n.t('meta.done', { user: message.reply_to_message.from }), answerOptions)
-  }
-}
+module.exports = optional(({ message }) => message && message.reply_to_message,
+  ({ message, reply, i18n }) => reply(i18n.t('meta.done'), Extra.inReplyTo(message.reply_to_message.message_id))
+)
