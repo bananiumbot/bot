ad1546b874202969c6ae1d3a1835ed563f9ebf7e
Bunk100 <37146584+Bunk100@users.noreply.github.com>
2019-08-17 02:08:58 +0300

    Update restrict-user.js
    #оттепель 2.0

--- admin/restrict-user.js
+++ admin/restrict-user.js
@@ -5,5 +5,5 @@ module.exports = async ({ message, reply, i18n, restrictChatMember }) => {
   const now = Math.floor(new Date().getTime() / 1000)
   const targetUser = message.reply_to_message.from
   await reply(i18n.t('admin.user-restricted', { user: targetUser }))
-  await restrictChatMember(targetUser.id, { until_date: now + 60 * 15 })
+  await restrictChatMember(targetUser.id, { until_date: now + 60 * 5 })
 }
