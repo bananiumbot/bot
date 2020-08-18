407b665c161aa92914c166a1cc21bf04c4d0579f
Vitaly Domnikov <dotcypress@gmail.com>
2020-08-17 18:06:11 -0700

    error hadling

--- api/bot.js
+++ api/bot.js
@@ -2,7 +2,7 @@ const Telegraf = require('telegraf')
 const Bananium = require('../bananium')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.catch(console.log)
+bot.catch((err) => console.log(err.message, err))
 bot.use(Bananium)
 
 module.exports = bot.webhookCallback(`/api/bot?secret=${process.env.SECRET}`)
