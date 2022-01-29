1ccd542a90d3faaff5c1502ff09ef2a9d30ad13c
Vyacheslav <slava021323@gmail.com>
2022-01-29 22:09:44 +0600

    Increase ban max period value up to 24h

--- common/ban-caller.js
+++ common/ban-caller.js
@@ -3,7 +3,7 @@ const { Extra } = require('telegraf')
 
 module.exports = async (ctx) => {
   const now = Math.round(Date.now() / 1000)
-  const period = Math.round(Math.random() * 50) + 10
+  const period = Math.round(Math.random() * 1430) + 10
   const until = now + 60 * period
   ctx.restrictChatMember(ctx.from.id, { until_date: until })
   const answer = ctx.i18n.t('self-ban', { period })
